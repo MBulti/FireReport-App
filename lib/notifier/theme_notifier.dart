a4bd8067cb6be87b224eaf253d0c5d73b619bce1
MBulti <m.bulthaup.development@gmail.com>
2024-10-24 14:48:41 +0200

    save theme

--- lib/notifier/theme_notifier.dart
+++ lib/notifier/theme_notifier.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.light);
+  ThemeNotifier() : super(ThemeMode.light) {
+    _loadThemeMode();
+  }
 
-  void toggleTheme() {
+  Future<void> _loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeIndex = prefs.getInt('themeMode') ?? ThemeMode.light.index;
+    state = ThemeMode.values[themeIndex];
+  }
+
+  Future<void> toggleTheme() async {
     state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('themeMode', state.index);
   }
 }
 
