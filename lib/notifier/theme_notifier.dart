ce94ed3d6ea62d6a91d15a3640d0467bf333ad28
MBulti <m.bulthaup.development@gmail.com>
2024-11-05 21:35:40 +0100

    darktheme default

--- lib/notifier/theme_notifier.dart
+++ lib/notifier/theme_notifier.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.light) {
+  ThemeNotifier() : super(ThemeMode.dark) {
     _loadThemeMode();
   }
 
